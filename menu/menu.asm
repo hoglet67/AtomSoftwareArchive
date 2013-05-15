9843992d3d681c983c7e9e1438bd9c16dd462077
David Banks <dbanks@hp.com>
2013-05-15 21:14:33 +0100

    Implemented items counts on filter selection screens
    Change-Id: Ica9cc82dd6f07adbf4d86867c759cfa4fbc65acd

--- menu/menu.asm
+++ menu/menu.asm
@@ -10,8 +10,11 @@
 	ShortPublisherTablePtr = MenuTableBase + 2;	
 	PublisherTablePtr = MenuTableBase + 4;	
 	GenreTablePtr = MenuTableBase + 6;	
-	CollectionTablePtr = MenuTableBase + 8;	
+	CollectionTablePtr = MenuTableBase + 8;
+	
+	CountBuffer = $100;
 
+	CountOffset = 0
 	PubIdOffset = 2
 	TitleNameOffset = 4
 	
@@ -39,13 +42,19 @@
 	; These are working values
 	Title = $82
 	ShortPub = $84
-	ShortPubName = $86
+	AnnotationString = $86
 	Screen = $88
 	TmpY = $8a    
 	RowCount =$90
 	Key = $80
-
-
+	
+	; For the Decimal Output routines
+	BinBuffer = $94
+	SuppressFlag = $96
+	BcdBuffer = $97
+	CountString = $800
+	
+	
 	; Miscellaneous constants
 	Space = $20
 	Return = $0d
@@ -262,21 +271,40 @@
 
 .WriteLine3
     LDA Annotation
-    BMI WriteLine5
+    BPL WriteShortPub
+
+	LDY #CountOffset
+	LDA (Title),Y
+	STA BinBuffer
+	INY
+	LDA (Title),Y
+	STA BinBuffer + 1		
+	JSR WriteDecimal
+
+	LDA #<CountString
+	STA AnnotationString
+	LDA #>CountString
+	STA AnnotationString + 1
+	
+	JMP WriteAnnotation
+
+.WriteShortPub 
 	LDY #PubIdOffset
 	LDA (Title),Y
 	CLC
 	ASL A
 	TAY
 	LDA (ShortPub),Y
-	STA ShortPubName
+	STA AnnotationString
 	INY
 	LDA (ShortPub),Y
-	STA ShortPubName + 1
+	STA AnnotationString + 1
+
+.WriteAnnotation
 	LDY #0
 
 .WriteLine4
-	LDA (ShortPubName),Y
+	LDA (AnnotationString),Y
 	CMP #Return
 	BEQ WriteLine5
 	DEX
@@ -290,17 +318,16 @@
 	JSR WriteToScreen
 	DEX
 	BNE WriteLine6
-	LDA Annotation
-	BMI WriteLine8
-	LDY #0
 
+	LDY #0
 .WriteLine7
-	LDA (ShortPubName),Y
+	LDA (AnnotationString),Y
 	CMP #Return
 	BEQ WriteLine8
 	JSR WriteToScreen 
 	INY
 	BNE WriteLine7
+
 .WriteLine8	
 	RTS
 
@@ -332,7 +359,84 @@
 	RTS
 
 
+	; Converts the 16-bit value in $BinBuffer to "(" <Decimal String> ")" <CR> at Buffer
 
+.WriteDecimal:
+	TXA
+	PHA
+	SED
+	LDA #0
+	STA BcdBuffer
+	STA BcdBuffer+1
+	STA BcdBuffer+2
+	; Handle the binary bits one at a time
+	LDY #16        
+.BcdLoop:
+    ASL BinBuffer
+	ROL BinBuffer+1
+	; Add into the BCD accumulator
+	LDA BcdBuffer
+	ADC BcdBuffer
+	STA BcdBuffer
+	LDA BcdBuffer+1
+	ADC BcdBuffer+1
+	STA BcdBuffer+1
+	LDA BcdBuffer+2
+	ADC BcdBuffer+2
+	STA BcdBuffer+2
+	DEY
+	BNE BcdLoop
+	CLD
+
+	; Set the flag to support suppression of leading zeros
+	STY SuppressFlag
+
+	; Output the BcdBuffer digits, MS first
+	LDA #'('
+	STA CountString
+	LDX #1
+	LDY #2
+.DecLoop
+	LDA BcdBuffer,Y
+	JSR WriteHex
+	DEY
+	BPL DecLoop
+	LDA #')'
+	STA CountString,X
+	INX
+	LDA #Return
+	STA CountString,X
+	PLA
+	TAX
+	RTS
+
+.WriteHex
+	PHA
+	LSR A
+	LSR A
+	LSR A
+	LSR A
+	JSR WriteHex1
+	PLA
+.WriteHex1
+	AND #$0f
+	BNE WriteHex2
+	; Suppress leading zero
+	BIT SuppressFlag
+	BPL WriteHex4
+.WriteHex2
+	; Make sure bit 7 of SuppressFlag is set, so we don't suppress further zeros
+	SEC
+	ROR	SuppressFlag
+	CMP #$0a
+	BCC WriteHex3	
+	ADC #$06
+.WriteHex3
+	ADC #$30
+	STA CountString,X
+	INX
+.WriteHex4
+	RTS
 
 .ENDOF
 
