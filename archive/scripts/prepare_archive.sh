6395b6fc06d9ced2fc15f1f374af45e90ec609c1
David Banks <dave@hoglet.com>
2020-10-21 17:54:31 +0100

    Re-instate BB backgammon until PCW items added to Menu
    Change-Id: I297bab85cf2b491c985953d72042624957752414

--- archive/scripts/prepare_archive.sh
+++ archive/scripts/prepare_archive.sh
@@ -339,7 +339,7 @@ package "atms/dd-21/SENTD" "$ARCHIVE/$AS/WORDTUT"
 package "atms/Acl1-15/747" "$ARCHIVE/$BB"
 # package "atms/Acl1-06/BREAKOU" "$ARCHIVE/$BB" # not bugbyte
 # package "atms/Acl1-15/INVADBB" "$ARCHIVE/$BB" # manually fixed
-# package "atms/Acl1-03/BACKGAM" "$ARCHIVE/$BB" # not sure this is bug byte, but could be
+package "atms/Acl1-03/BACKGAM" "$ARCHIVE/$BB" # not sure this is bug byte, but could be
 # package "atms/Acl1-21/BATSHIP" "$ARCHIVE/$BB" # this is actually a program called space battle that need FP Rom
 package "atms/Acl2-19/BIORYTH" "$ARCHIVE/$BB"
 #package "atms/bugbyte__chess_disk/*" "$ARCHIVE/$BB"
